8695d42c5bd3b42ae6de2e55615f991b56157d6b
rerrl <rerrl@zzyzxlabs.xyz>
2023-11-20 11:01:45 -0800

    implement cronjob structure

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
 import axios from 'axios';
 
+const COINGECKO_API_URL = 'https://api.coingecko.com/api/v3';
+const CRON_INTERVAL_S = 5;
+
 @Injectable()
-export class AppService {
+export class AppService implements OnModuleInit {
+  onModuleInit(): void {
+    this.updateCoins();
+  }
+
   async healthCheck(): Promise<string> {
-    const resp = await axios.get('https://api.coingecko.com/api/v3/ping');
+    const resp = await axios.get(`${COINGECKO_API_URL}/ping`);
     if (resp.status !== 200) {
       throw new Error('Error');
     }
     return 'OK';
+  }
+
+  @Cron(`*/${CRON_INTERVAL_S} * * * * *`)
+  async updateCoins(): Promise<void> {
+    console.log('Updating coins');
   }
 }
