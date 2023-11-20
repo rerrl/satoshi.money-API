9396a1ca8a2af05514e6d3a3c15a811165677c02
rerrl <rerrl@zzyzxlabs.xyz>
2023-11-20 12:08:45 -0800

    add coin upsert on interval

--- README.md
+++ README.md
@@ -13,8 +13,16 @@
 install deps
 `npm i`
 
-Make sure to set up your .env vars. You will need to provide your coingecko API key and db connection
+Make sure to set up your .env vars. You will need to provide your db connection credentials. I will send this over in an email
+
 ```
 DATABASE_URL=""
-COINGECKO_API_KEY=
+UPDATE_COIN_CRON_INTERVAL_S=120
 ```
+
+To run locally while interfacing with the deployed elephant db:
+`npm run start:dev`
+
+You are allowed 30 requests/min, and by default I have the update interval every 2 mins. You shouldnt have a problem with (429 - too many requests) but if you do, change you VPN location ;)
+
+To keep this simple for now, we are only concerned with updating the top 100 coins, sorted by market cap.
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,17 +1,22 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
+import { PrismaClient } from '@prisma/client';
 import axios from 'axios';
 
-const COINGECKO_API_URL = 'https://api.coingecko.com/api/v3';
-const CRON_INTERVAL_S = 5;
+const COINGECKO_API_URL = process.env.COINGECKO_API_URL;
+const prisma = new PrismaClient();
 
 @Injectable()
 export class AppService implements OnModuleInit {
   onModuleInit(): void {
-    this.updateCoins();
+    // if not testing with jest:
+    if (process.env.NODE_ENV !== 'test') {
+      this.updateCoins();
+    }
   }
 
   async healthCheck(): Promise<string> {
+    console.log('Health check...');
     const resp = await axios.get(`${COINGECKO_API_URL}/ping`);
     if (resp.status !== 200) {
       throw new Error('Error');
@@ -19,8 +24,52 @@ export class AppService implements OnModuleInit {
     return 'OK';
   }
 
-  @Cron(`*/${CRON_INTERVAL_S} * * * * *`)
+  @Cron(`*/${process.env.UPDATE_COIN_CRON_INTERVAL_S} * * * * *`, {
+    disabled: process.env.NODE_ENV === 'test',
+  })
   async updateCoins(): Promise<void> {
-    console.log('Updating coins');
+    try {
+      console.log('Updating coins...');
+      const coins = await axios.get(`${COINGECKO_API_URL}/coins/markets`, {
+        params: {
+          vs_currency: 'usd',
+          order: 'market_cap_desc',
+          per_page: 100,
+          page: 1,
+          sparkline: false,
+          locale: 'en',
+        },
+      });
+      for (const coin in coins.data) {
+        await prisma.coins.upsert({
+          where: { cg_id: coins.data[coin].id },
+          update: {
+            current_price: coins.data[coin].current_price,
+            market_cap: coins.data[coin].market_cap,
+            high_24h: coins.data[coin].high_24h,
+            low_24h: coins.data[coin].low_24h,
+            ath: coins.data[coin].ath,
+          },
+          create: {
+            cg_id: coins.data[coin].id,
+            symbol: coins.data[coin].symbol,
+            name: coins.data[coin].name,
+            current_price: coins.data[coin].current_price,
+            market_cap: coins.data[coin].market_cap,
+            mc_rank: coins.data[coin].market_cap_rank,
+            high_24h: coins.data[coin].high_24h,
+            low_24h: coins.data[coin].low_24h,
+            ath: coins.data[coin].ath,
+            volume_24h: coins.data[coin].total_volume,
+            last_updated: coins.data[coin].last_updated,
+          },
+        });
+      }
+    } catch (error) {
+      console.error(error.message);
+    } finally {
+      console.log('Done updating coins.');
+      await prisma.$disconnect();
+    }
   }
 }
